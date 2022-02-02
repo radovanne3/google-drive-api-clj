45ce13e9da98f99f088fe785e605cd7bd19de823
Snorlax <radovanne3@gmail.com>
2022-02-02 13:44:07 +0100

    added list-files

--- src/google_drive_api_clj/constants.clj
+++ src/google_drive_api_clj/constants.clj
@@ -29,15 +29,10 @@
 ;;        );
 
 (defn ^ServiceAccountCredentials credential-with-scopes
-  "Creates a copy of the given credential, with the specified scopes attached.
-  `scopes` should be a list or vec of one or more Strings"
   [^ServiceAccountCredentials credentials, scopes]
   (.createScoped credentials (list scopes)))
 
 (defn default-credential
-  "Gets the default credential as configured by the GOOGLE_APPLICATION_CREDENTIALS environment variable
-  (see https://developers.google.com/identity/protocols/application-default-credentials)
-  Optionally you may specify a collection (list/vec/set) of string scopes to attach to the credential"
   ([scopes]
    (credential-with-scopes (ServiceAccountCredentials/fromStream (new FileInputStream CREDENTIALS_FILE_PATH)) scopes)))
 
@@ -53,6 +48,10 @@
       (.setApplicationName APPLICATION_NAME)
       .build))
 
+(-> (Drive$Builder. HTTP_TRANSPORT JSON_FACTORY requestInitializer)
+    (.setApplicationName APPLICATION_NAME)
+    .build)
+
 (defn list-files
   "List first n files, second argument will be number of file names and file IDs you want to return"
   []
@@ -63,9 +62,11 @@
                   (.setFields "nextPageToken, files(id, name)")
                   .execute
                   .getFiles)]
-    (if (and files (pos? (.size files)))
-      (map (juxt #(.getName %) #(.getId %)) files)
-      "No files or folders found")))
+    (if (empty? files)
+      (println "No files were found.")
+      (map (fn [x]
+             (do (println (str "File name:" (.getName x)))
+                 (println (str "File ID:" (.getId x))))) files))))
 
 (list-files)
 
